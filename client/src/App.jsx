2d7bdabc2af232274bbcce79d0c2b9bfdf2a7523
Kenwrick Espinosa <kenwrickespinosa@gmail.com>
2025-12-26 18:20:26 +0800

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <div>
+      <p className="bg-red-200">Hi App</p>
+    </div>
+  )
+}
+
+export default App
